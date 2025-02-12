abcde7c209192bc3005454ad6898b3d5fab2cf51
Mateo Espinosa <mateoespinosa7777@gmail.com>
2025-02-12 14:47:41 -0500

    first commit

---
+++ database/connection.php
@@ -0,0 +1,19 @@
+<?php
+
+    /*Configuración Base de datos*/
+    const SERVER = "localhost";
+    const DB = "taskmanagerdb";
+    const USER = "root";
+    const PASS = "";
+    const UTF8 = "utf8";
+
+    const SGBD = "mysql:host=".SERVER.";dbname=".DB.";charset=".UTF8;
+
+    class dbConnection{
+        protected function connection(){
+            $con = new PDO(SGBD, USER, PASSWORD);
+
+            return $con;
+        }
+    }
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestor de Tareas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <main class="container">
+        <section>
+            <form action="">
+                <div class="row">
+                    <div class="col-md-4">
+                        <input type="text" name="name" id="name" class="form-control">
+                    </div>
+                </div>
+            </form>
+        </section>
+    </main>
+</body>
+</html>
